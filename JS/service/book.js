a89b44c549b5136a68d3981a44dc4b68ed955006
Lioz Fired <friedlioz@gmail.com>
2025-03-17 17:46:45 +0200

    Update demo data more books and rating

--- JS/service/book.js
+++ JS/service/book.js
@@ -10,9 +10,16 @@ function _createBooks() {
     if (gBooks && gBooks.length > 0) return
 
     gBooks = [
-        { id: 'bg4J78', title: 'The Adventures of Lori Ipsi', price: 120, imgUrl: '/img/the adventure.jpeg' },
-        { id: 'xy5F92', title: 'World Atlas', price: 300, imgUrl: '/img/world atlas.jpeg' },
-        { id: 'ab2X55', title: 'Zorba the Greek', price: 87, imgUrl: '/img/zorba.png' },
+        { id: 'bg4J78', title: 'The Adventures of Lori Ipsi', price: 120, imgUrl: '/img/the adventure.jpeg', rating: 2 },
+        { id: 'xy5F92', title: 'World Atlas', price: 300, imgUrl: '/img/world atlas.jpeg', rating: 3 },
+        { id: 'ab2X55', title: 'Zorba the Greek', price: 87, imgUrl: '/img/zorba.png', rating: 1 },
+        { id: '3Tw45h', title: 'Long Shadows', price: 100, imgUrl: '/img/long shadows.jpeg', rating: 5 },
+        { id: 'DDf352', title: 'The Lord Of The Rings, The Two Towers', price: 350, imgUrl: '/img/the lord of the rings the two towers.jpeg', rating: 4 },
+        { id: 'Aqt72W', title: 'Shantaram', price: 200, imgUrl: '/img/shantaram.jpeg', rating: 4 },
+        { id: 'LI440F', title: 'The Hobbit', price: 80, imgUrl: '/img/the hobbit.png', rating: 2 },
+        { id: 'C4He31', title: 'End Game', price: 75, imgUrl: '/img/end game.jpeg', rating: 1 },
+        { id: '99YyX3', title: 'The Forgotten', price: 120, imgUrl: '/img/the forgotten.jpeg', rating: 5 },
+        { id: 'rT6o12', title: 'The Escape', price: 150, imgUrl: '/img/the escape.jpeg', rating: 3 },
     ]
     _saveBooks()
 }
